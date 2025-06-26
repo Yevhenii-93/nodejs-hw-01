fc82a7876f726370b861561b510414facb74e25a
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-06-27 01:16:47 +0300

    create read and write contacts utils

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require('node:fs/promises');
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const data = await fs.readFile(PATH_DB, { encoding: 'utf-8' });
+
+  const contacts = JSON.parse(data);
+  return contacts;
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,6 @@
 import { PATH_DB } from '../constants/contacts.js';
+const fs = require('node:fs/promises');
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  await fs.writeContacts(PATH_DB, JSON.stringify(updatedContacts));
+};
