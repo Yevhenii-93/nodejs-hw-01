59c6dd65477bf77eea7a09fb88e2dedddf5a0828
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-06-27 17:23:02 +0300

    fixed write contacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,5 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
-const fs = require('node:fs/promises');
+import * as fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
   await fs.writeContacts(PATH_DB, JSON.stringify(updatedContacts));
