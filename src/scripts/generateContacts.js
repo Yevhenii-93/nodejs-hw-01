725fd292c9678d719ac677b16b508a21d092b146
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-06-27 16:32:02 +0300

    create generate contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+  const updatedContacts = [...existingContacts, ...newContacts];
+  await writeContacts(updatedContacts);
+  const existingContacts = await readContacts();
+  console.log(`${number} контакт(ів) успішно згенеровано та додано.`);
+};
 
 generateContacts(5);
