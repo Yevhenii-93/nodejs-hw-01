4413ad6b7032cd4cd89d6fd4672533e1a8fa4cf9
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-06-27 18:57:45 +0300

    create removeLastContact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,12 @@
-export const removeLastContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+// import { readContacts } from '../utils/readContacts.js';
+import fs from 'node:fs/promises';
+
+export const removeLastContact = async () => {
+  fs.unlink(PATH_DB);
+
+  //   const contacts = await readContacts();
+  //   fs.unlink(contacts);
+};
 
 removeLastContact();
